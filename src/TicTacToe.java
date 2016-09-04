f32257cedf65a028eec812045944ee74682c6aac
Ktailor34 <tailorkishan123@gmail.com>
2016-09-04 15:32:23 -0400

    Merged Code

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -8,16 +8,22 @@ import java.util.Random;
 public class TicTacToe {
     Scanner scan = new Scanner(System.in);
     Random generator = new Random();
-    private int [][] board;
+    private int[][] board;
+    private int[][] playerBoard;
+    private int[][] computerBoard;
     private final int SIZE = 3;
 
 
+    // Constructor
     public TicTacToe() {
         board = new int[SIZE][SIZE];
-
+        playerBoard = new int[SIZE][SIZE];
+        computerBoard = new int[SIZE][SIZE];
         for(int r = 0; r < SIZE; r++) {
             for(int c = 0; c < SIZE; c++) {
                 board[r][c] = 0;
+                playerBoard[r][c] = 0;
+                computerBoard[r][c] = 0;
             }
         }
     }
@@ -26,46 +32,58 @@ public class TicTacToe {
             for (int r = 0; r < SIZE; r++) {
                 for (int c = 0; c < SIZE; c++) {
                     System.out.print(board[r][c] + " ");
-                    if (c  == 2 && r < 2) {
+                    if (c  == 2) {
                         System.out.println();
                     }
                 }
             }
         }
 
-    private void player_move() {
-
+    private void playerMove() {
+        boolean flag = true;
+        while(flag) {
+            System.out.print("Pick spots 1 - 9: ");
+            int selection = scan.nextInt();
+            if(selection > 0 && selection <=9) {
+                flag = false;
+                board[(selection - 1)/3][(selection - 1) % 3] = 1;
+                playerBoard[(selection -1)/3][(selection - 1) % 3] = 1;
+            }
+            else {
+                System.out.println("Incorrect input. Try again.");
+            }
+        }
     }
 
     private void computerMove() {
-    	int compPlacement = generator.nextInt(10);
-    	boolean moveMade = false;
-    	
-    	while (moveMade != true){
-    		if (board[(compPlacement)/3][(compPlacement) % 3] == 0 ) {
-    			board[(compPlacement)/3][(compPlacement) % 3] = 2;
-    			moveMade = true;
-    		} else {
-    			compPlacement = generator.nextInt(10);
-    		} // end of if
-    	} // end of while
+        int compPlacement = generator.nextInt(10);
+        boolean moveMade = false;
+
+        while (moveMade != true){
+            if (board[(compPlacement)/3][(compPlacement) % 3] == 0 ) {
+                board[(compPlacement)/3][(compPlacement) % 3] = 2;
+                moveMade = true;
+            } else {
+                compPlacement = generator.nextInt(10);
+            } // end of if
+        } // end of while
+    }
+
+    private boolean gameOver() {
+        return false;
     }
 
     public void play() {
-         System.out.print(" \n Pick spots 1 - 9: ");
-        int selection = scan.nextInt();
-        board[(selection - 1)/3][(selection - 1) % 3] = 1;
+        while(!gameOver()) {
+            drawBoard();
+            playerMove();
+            computerMove();
+        }
     }
 
 	public static void main(String args[]) {
         TicTacToe myGame = new TicTacToe();
-        boolean done = false;
-
-        while (done != true){
-        	myGame.drawBoard();
-        	myGame.play();
-            myGame.computerMove();
-            
-        }
+        myGame.play();
     }
 }
+
